2884c2097d84064cb4103213b8af21d5ba753686
Paolo Piaggesi <paolo@Paolos-MacBook-Air.local>
2017-02-13 23:55:37 +0000

    error handler for getAll tips from API

--- Tips.Web.Service/wwwroot/js/app.js
+++ Tips.Web.Service/wwwroot/js/app.js
@@ -15,7 +15,11 @@ myApp.config(function($routeProvider, RestangularProvider) {
 
 // Controller for show a tip
 myApp.controller('ShowTipController', ['$scope', 'Restangular', function($scope, Restangular) {
-    $scope.tips = Restangular.all("tips").getList().then(function(tips){
-        $scope.tipToShow = tips[Math.floor(Math.random() * tips.length)];
-    })
+    $scope.tips = Restangular.all("tips").getList()
+        .then(function(tips){
+            $scope.tipToShow = tips[Math.floor(Math.random() * tips.length)];
+        },
+        function(err){
+            $scope.tipToShow = {text:"maybe there is a problem with the tips api :-)"};
+        });
 }]);
