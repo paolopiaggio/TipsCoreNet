bb641fa063a162dfd1a979e3542a51fc0e99c6a2
Paolo Piaggesi <paolo@Paolos-MacBook-Air.local>
2017-05-15 23:20:46 +0100

    Updated FW to NetCore 1.1 and added command line arguments to API config

--- Tips.Api.Service/Program.cs
+++ Tips.Api.Service/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Tips.Api.Service
 {
@@ -12,7 +13,12 @@ namespace Tips.Api.Service
     {
         public static void Main(string[] args)
         {
+            IConfiguration config  = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
